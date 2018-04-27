6c9e6c39c46b7d7dbb903537a50c4464e3cb52ca
pushpakumara.14@cse.mrt.ac.lk <pushpakumara.14@cse.mrt.ac.lk>
2018-04-28 00:45:54 +0530

    initial

---
+++ src/AppBundle/Controller/DashboardController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace AppBundle\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends DefaultController
+{
+
+    /**
+     * @Route("/", name="search")
+     */
+    public function searchAction(Request $request){
+        $fuel = $request->get('fuel');
+        $wheels = $request->get('wheels');
+        $doors = $request->get('doors');
+        $price = explode(",",$request->get('price'));
+        $speed = explode(",",$request->get('speed'));
+        $em = $this->getDoctrine()->getManager();
+
+        $cars = $em->getRepository('AppBundle:Car')->search($fuel,$wheels,$price,$doors,$speed);
+        return $this->render("default/search.html.twig",array(
+            'fuel'=>$fuel,
+            'wheels'=>$wheels,
+            'price'=>$price,
+            'speed'=>$speed,
+            'doors'=>$doors,
+            'cars'=>$cars
+        ));
+    }
+
+    /**
+     * @Route("/dashboard", name="dashboard")
+     */
+    public function dashboardAction(Request $request){
+        return $this->redirectToRoute('product_list');
+    }
+
+
+
+
+
+}
